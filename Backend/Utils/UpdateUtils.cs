628c0c7097f7ad64f3d2d34afcd671c302cfc3a4
Segergren <olle.segergren@gmail.com>
2025-02-27 14:54:37 +0100

    fix: update token issue

--- Backend/Utils/UpdateUtils.cs
+++ Backend/Utils/UpdateUtils.cs
@@ -12,16 +12,15 @@ namespace Segra.Backend.Utils
     {
         // Store the update information
         public static UpdateInfo? LatestUpdateInfo { get; private set; } = null;
+        private static string Token = "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"; // Read access to Segra. Remove when Public
         public static GithubSource Source = new GithubSource("https://github.com/Segergren/Segra", Token, false);
         public static UpdateManager? UpdateManager { get; private set; } = new UpdateManager(Source);
-        private static string Token = "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";
-
+        
         public static async Task<bool> UpdateAppIfNecessary()
         {
             try
             {
                 Log.Information("Checking if update is necessary");
-                // Read access to Segra. Remove when Public
                 UpdateInfo newVersion = await UpdateManager.CheckForUpdatesAsync();
 
                 if (newVersion == null)
