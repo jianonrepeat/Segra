9b5a69cb4b185429fd557c4ac53df629b4cf50de
Segergren <olle.segergren@gmail.com>
2025-08-23 01:38:02 +0200

    feat: improve release notes modal

--- Frontend/src/Components/ReleaseNotesModal.tsx
+++ Frontend/src/Components/ReleaseNotesModal.tsx
@@ -1,6 +1,7 @@
 import { ReleaseNote } from '../Models/WebSocketMessages';
 import Markdown from 'markdown-to-jsx';
 import { useContext, useEffect, useState } from 'react';
+import { gt } from 'semver';
 import { ReleaseNotesContext } from '../App';
 
 interface ReleaseNotesModalProps {
@@ -66,34 +67,9 @@ const MarkdownComponents = {
   img: (props: any) => <img className="markdown-content-img" {...props} />,
 };
 
-/**
- * Compares two semantic version strings to determine if version2 is newer than version1.
- * @param version1 - First version string (e.g., "1.2.3")
- * @param version2 - Second version string (e.g., "1.3.0")
- * @returns true if version2 is newer than version1, false otherwise
- */
+// Returns true if version2 is newer than version1 using semver precedence (handles -rc.x correctly)
 function isVersionNewer(version1: string, version2: string): boolean {
-  // Split versions into components
-  const v1Parts = version1.split('.').map(part => parseInt(part, 10));
-  const v2Parts = version2.split('.').map(part => parseInt(part, 10));
-  
-  // Ensure both arrays have the same length (typically 3 for semantic versioning)
-  while (v1Parts.length < 3) v1Parts.push(0);
-  while (v2Parts.length < 3) v2Parts.push(0);
-  
-  // Compare major version
-  if (v2Parts[0] > v1Parts[0]) return true;
-  if (v2Parts[0] < v1Parts[0]) return false;
-  
-  // Compare minor version
-  if (v2Parts[1] > v1Parts[1]) return true;
-  if (v2Parts[1] < v1Parts[1]) return false;
-  
-  // Compare patch version
-  if (v2Parts[2] > v1Parts[2]) return true;
-  
-  // If we get here, versions are equal or v2 is older
-  return false;
+  return gt(version2, version1, { loose: true });
 }
 
 export default function ReleaseNotesModal({ onClose, filterVersion }: ReleaseNotesModalProps) {
